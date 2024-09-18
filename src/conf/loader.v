1e32efe6046835c7aab6e9290f29e21430602863
SyoBoN <syobon@syobon.net>
2024-09-18 18:09:18 +0900

    generate new key if privkey_seed is not valid

--- src/conf/loader.v
+++ src/conf/loader.v
@@ -37,7 +37,7 @@ pub:
 	pub:
 		root string @[required]
 	}
-	user struct {
+	user     struct {
 		UserConfig
 	pub:
 		acct_full   string             @[required]
@@ -67,7 +67,15 @@ fn read_config() Data {
 	config_json := os.read_file('config.json') or { panic('Failed to read config.json: ${err}') }
 	config := json.decode(Config, config_json) or { panic('Failed to parse config.json: ${err}') }
 	manifest := vmod.decode(@VMOD_FILE) or { panic(err) }
-	privkey := ed25519.new_key_from_seed(hex.decode(config.user.privkey_seed) or { panic(err) })
+	seed_u8 := hex.decode(config.user.privkey_seed) or { panic(err) }
+	if seed_u8.len != ed25519.seed_size {
+		println('Generating a private key...')
+		_, new_key := ed25519.generate_key() or { panic(err) }
+		println('A key has been generated. Please append the following to your config:')
+		println('"privkey_seed": "${hex.encode(new_key.seed())}"')
+		exit(0)
+	}
+	privkey := ed25519.new_key_from_seed(seed_u8)
 
 	return Data{
 		software: manifest
@@ -75,7 +83,7 @@ fn read_config() Data {
 			ServerConfig: config.server
 			root:         'https://${config.server.host}'
 		}
-		user: struct {
+		user:     struct {
 			UserConfig:  config.user
 			acct_full:   '${config.user.acct}@${config.server.host}'
 			actor_url:   'https://${config.server.host}/actor'
